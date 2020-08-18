ab684bd12eba4eb0e60f32ac0d715464a2076d9f
Felipe Silva Aguiar <t_faguiar@grupodisal.com.br>
2020-08-17 21:23:30 -0300

    feat: projeto criado

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
